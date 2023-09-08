256ff0e9d800e2f1684c687feb03da3059310ae2
Nadia Ligia <nlnadialigia@gmail.com>
2023-09-08 18:49:07 -0300

    clear app structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
         <h1>Hello World</h1>
-      </header>
-    </div>
   );
 }
 
