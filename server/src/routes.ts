da7ad5e8394146bd47097fa2d1254bc8017751b1
Nadia Ligia <nlnadialigia@gmail.com>
2023-09-07 23:10:24 -0300

    create url image

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,23 @@
+import "dotenv/config";
 import {Router} from "express";
+import {connection} from "./database/connection";
+
 
 const routes = Router();
 
-routes.get("/", (request, response) => {
-  return response.json({message: "Hello World"})
+const url = process.env.URL
+
+routes.get("/items", async (request, response) => {
+  const items = await connection("items").select("*")
+
+  const serializedItems = items.map(item => {
+    return {
+      tittle: item.tittle,
+      image_url: `${url}/uploads/${item.image}`
+    }
+  })
+
+  return response.json(serializedItems)
 });
 
 export {routes};
