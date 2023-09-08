e76032cd43a8d033d7b672778411d0a0789e77db
Nadia Ligia <nlnadialigia@gmail.com>
2023-09-07 23:40:11 -0300

    create collect point

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -12,6 +12,7 @@ routes.get("/items", async (request, response) => {
 
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       tittle: item.tittle,
       image_url: `${url}/uploads/${item.image}`
     }
@@ -19,5 +20,35 @@ routes.get("/items", async (request, response) => {
 
   return response.json(serializedItems)
 });
+
+routes.post("/points",async (request, response) => {
+  const {name, email, whatsapp, latitude, longitude, city, uf, items} = request.body
+
+  const trx = await connection.transaction()
+
+  const insertedIds = await trx("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  })
+
+  const point_id = insertedIds[0]
+
+  const pointItems = items.map((item_id: number)=> {
+    return {
+      item_id,
+      point_id
+    }
+  })
+
+  await trx("point_items").insert(pointItems)
+
+  return response.status(200).json({message: "Successfully registered collection point!"})
+})
 
 export {routes};
