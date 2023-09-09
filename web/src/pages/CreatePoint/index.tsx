d54f786f6560fc197d812e7fae2d42c931c6388c
Nadia Ligia <nlnadialigia@gmail.com>
2023-09-09 18:26:04 -0300

    add uload file

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -3,6 +3,7 @@ import React, {ChangeEvent, FormEvent, useEffect, useState} from 'react';
 import {FiArrowLeft} from 'react-icons/fi';
 import {Link} from 'react-router-dom';
 import logo from "../../assets/logo.svg";
+import {Dropzone} from '../../components/Dropzone';
 import {Map} from '../../components/Map';
 import {api} from '../../services/api';
 import './styles.css';
@@ -21,6 +22,12 @@ interface IBGECityResponse {
 }
 
 const CreatePoint: React.FC = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    whatsapp: ""
+  })
+
   const [items, setItems] = useState<Item[]>([])
   const [ufs, setUfs] = useState<string[]>([])
   const [cities, setCities] = useState<string[]>([])
@@ -28,11 +35,7 @@ const CreatePoint: React.FC = () => {
   const [selectedCity, setSelectedCity] = useState("0")
   const [selectedMap, setSelectedMap] = useState<number[]>([])
   const [selectItems, setSelectItems] = useState<number[]>([])
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    whatsapp: ""
-  })
+  const [selectFile, setSelectFile] = useState<File>()
 
 
   useEffect(() => {
@@ -104,7 +107,20 @@ const CreatePoint: React.FC = () => {
     const [latitude, longitude] = selectedMap
     const items = selectItems
 
-    const data = {name, email, whatsapp, uf, city, latitude, longitude, items}
+    const data = new FormData()
+
+    data.append("name", name)
+    data.append("email", email)
+    data.append("whatsapp", whatsapp)
+    data.append("uf", uf)
+    data.append("city", city)
+    data.append("latitude", String(latitude))
+    data.append("longitude", String(longitude))
+    data.append("items", items.join(","))
+
+    if (selectFile) {
+      data.append("image", selectFile)
+    }
 
     await api.post("points", data)
 
@@ -123,6 +139,8 @@ const CreatePoint: React.FC = () => {
 
         <form onSubmit={handleSubmit}>
           <h1>Cadastro do <br />ponto de coleta</h1>
+
+          <Dropzone onFileUploaded={setSelectFile} />
 
           <fieldset>
             <legend><h2>Dados</h2></legend>
